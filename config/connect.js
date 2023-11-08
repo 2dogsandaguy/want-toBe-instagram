49ac18f869cc90ad88fe5d21147c18314dc296b0
2dogsandaguy <Neverclear@ymail.com>
2023-11-08 18:28:39 -0500

    stuff

--- config/connect.js
+++ config/connect.js
@@ -9,6 +9,6 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI|| 'mongodb://127.0.0.1:27017/social-network';
 
-connect(connectionString);
+mongoose.connect(connectionString);
 
 module.exports = connection;
