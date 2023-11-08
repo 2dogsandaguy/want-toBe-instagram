3a91c241c47defa25c47c2f3ed5969a9f3ab7d36
2dogsandaguy <Neverclear@ymail.com>
2023-11-08 18:31:48 -0500

    stuff

--- config/connect.js
+++ config/connect.js
@@ -9,6 +9,12 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI|| 'mongodb://127.0.0.1:27017/social-network';
 
-connect(connectionString);
+connect(connectionString)
+.then(() => {
+  console.log('MongoDB connected successfully');
+})
+.catch((error) => {
+  console.error('MongoDB connection error:', error);
+});
 
 module.exports = connection;
